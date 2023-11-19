fa24a6deb465085f4e8dcc7eaef4fa1e15bbdeb2
amundead <150055552+amundead@users.noreply.github.com>
2023-11-19 16:37:02 +0000

    s

--- app/config.php
+++ app/config.php
@@ -4,7 +4,7 @@ server with default setting (user 'root' with no password) */
 
 $hostname = (getenv('MYSQL_HOST'));
 $user = (getenv('MYSQL_USER'));
-$password = (getenv('MYSQL_PASSWORD'));
+$password = (getenv('MYSQL_ROOT_PASSWORD'));
 $database = (getenv('MYSQL_DATABASE'));
 
 
