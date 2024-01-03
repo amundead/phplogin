f31dd2c2a9a70354b155985e6d6d155b7f1c563f
amundead <150055552+amundead@users.noreply.github.com>
2024-01-03 11:48:09 +0800

    update

--- app/login.php
+++ app/login.php
@@ -102,7 +102,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 <body>
     <div class="wrapper">
         <h2>Login :)</h2>
-        <p>Please fill in your credentials to login.</p>
+        <p>test update.</p>
 
         <?php 
         if(!empty($login_err)){
